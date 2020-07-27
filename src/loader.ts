92762604549e0bcf8ca47b20ba2e61f5e4df8878
gregk27 <git@gregk.ca>
2020-07-27 18:41:14 -0400

    Updated loader code

--- src/loader.ts
+++ src/loader.ts
@@ -1,50 +1,25 @@
-import * as fs from 'fs';
-import { Subsystem, Command } from './treeType';
-import { parse } from './javaParser/javaParser';
+import { Subsystem, Command } from "./treeType";
+import { parseFolder } from "./javaParser/javaParser";
+import { JavaClass } from "./javaParser/JavaClasses";
 
-export var subsystems:Subsystem[] = [];
-export var commands:Command[] = [];
-let basePath = "";
-
-export async function load(workspaceRoot:string){
+export async function load(workspaceRoot:string): Promise<{subsystems: Subsystem[], commands: Command[]}> {
     console.log("Loading");
-    basePath = workspaceRoot+"/src/main/java/";
-    subsystems = [];
-    commands = [];
+    let subsystems:Subsystem[] = [];
+    let commands:Command[] = [];
 
-    await(parseDir(basePath, fs.readdirSync(basePath)));
+    await parseFolder(workspaceRoot+"/build/classes/java/main/", workspaceRoot+"/src/main/java/", true, (cls:JavaClass)=>{
+        if(cls.superClass.full === "edu/wpi/first/wpilibj2/command/SubsystemBase"){
+            subsystems.push(new Subsystem(cls));
+        }else if (cls.superClass.full === "edu/wpi/first/wpilibj2/command/CommandBase" && cls.pckg.includes("auto")){
+            commands.push(new Command(cls, Command.AUTO));
+        } else if (cls.superClass.full === "edu/wpi/first/wpilibj2/command/InstantCommand" && cls.pckg.includes("auto")){
+            commands.push(new Command(cls, Command.AUTO | Command.INSTANT));
+        } else if (cls.superClass.full === "edu/wpi/first/wpilibj2/command/CommandBase"){
+            commands.push(new Command(cls));
+        } else if (cls.superClass.full === "edu/wpi/first/wpilibj2/command/InstantCommand"){
+            commands.push(new Command(cls, Command.INSTANT));
+        } 
+    });
     console.log("Done");
-}
-
-
-async function parseDir(root:string, files: string[]){
-    console.log("Parsing");
-    console.log(root);
-    console.log(files);
-    if(files === undefined){
-        console.log("files undefined");
-        return;
-    }
-    let promises:Promise<void>[] = [];
-    for(let fileName of files){
-        let filePath = root+fileName;
-        if(fs.lstatSync(filePath).isDirectory()){
-            promises.push(parseDir(filePath+"/", fs.readdirSync(filePath)));
-        } else if (filePath.endsWith(".java")) {
-            let content = fs.readFileSync(filePath).toString();
-            let cls = parse(basePath, filePath.replace("src/main/java/", "build/classes/java/main/").replace(".java", ".class"));
-            if(cls.superClass === "edu/wpi/first/wpilibj2/command/SubsystemBase"){
-                subsystems.push(new Subsystem(cls));
-            }else if (cls.superClass === "edu/wpi/first/wpilibj2/command/CommandBase" && cls.pckg.includes("auto")){
-                commands.push(new Command(cls, Command.AUTO));
-            } else if (cls.superClass === "edu/wpi/first/wpilibj2/command/InstantCommand" && cls.pckg.includes("auto")){
-                commands.push(new Command(cls, Command.AUTO | Command.INSTANT));
-            } else if (cls.superClass === "edu/wpi/first/wpilibj2/command/CommandBase"){
-                commands.push(new Command(cls));
-            } else if (cls.superClass === "edu/wpi/first/wpilibj2/command/InstantCommand"){
-                commands.push(new Command(cls, Command.INSTANT));
-            } 
-       }
-    }
-    await Promise.all(promises);
+    return {subsystems, commands};
 }
