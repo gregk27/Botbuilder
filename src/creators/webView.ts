b5dc439dcad743a1c1fcfeee69664daabc365800
gregk27 <git@gregk.ca>
2020-07-28 12:40:23 -0400

    Added code to replace css file references

--- src/creators/webView.ts
+++ src/creators/webView.ts
@@ -1,5 +1,8 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import { getServers } from 'dns';
+
+const cssPattern = /<link\s*rel=['"]stylesheet['"]\s*type="text\/css"\s*href=['"](\w*\.css)['"]\s*>/g;
 
 export abstract class WebviewBase {
 
@@ -10,12 +13,14 @@ export abstract class WebviewBase {
         context: vscode.ExtensionContext,
         public name:string,
         public title:string,
-        htmlFile:string = null
+        private filePath:string
     ) {
-        if(htmlFile !== null){
-            this.html = fs.readFileSync(context.extensionPath+"/resources/html/"+htmlFile).toString();
-            console.log(this.html);
-        }
+        this.filePath = context.extensionPath+"/resources/html/"+filePath;
+        this.html = fs.readFileSync(this.filePath).toString();
+        console.log(this.html);
+        cssPattern.exec(filePath);
+        
+
     }
 
     protected getHTML():string{
@@ -24,9 +29,31 @@ export abstract class WebviewBase {
 
     show(): vscode.WebviewPanel {
 		const panel = vscode.window.createWebviewPanel(this.name, this.title, vscode.ViewColumn.Active, this.options);
+        
+        this.html = this.html.replace(cssPattern, (substring, filename)=>{
+            console.log(substring, filename);
+            if(filename !== undefined){
+                return substring.replace(filename, this.getResource(filename, panel.webview));
+                // return "<style>\n"+fs.readFileSync(this.filePath.substring(0, this.filePath.lastIndexOf('/')+1)+filename)+"\n</style>";
+            }
+            return substring;
+        });
+
+        console.log(this.html);
         panel.webview.html = this.getHTML();
 
         return panel;
+    }
+    
+    private getResource(path:string, view: vscode.Webview): string{
+        let locOnDisk = vscode.Uri.file(this.filePath.substring(0, this.filePath.lastIndexOf('/')+1)+path);
+        console.log(this.filePath.substring(0, this.filePath.lastIndexOf('/')+1)+path);
+        return view.asWebviewUri(locOnDisk).toString();
+        
+    }
+
+    private replaceResource(oldPath:string, view: vscode.Webview): void{
+        this.html = this.html.replace(oldPath, this.getResource(oldPath, view));
     }
 
 }
