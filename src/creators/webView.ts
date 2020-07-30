e08b8b9af8b713c96f9ba5899e6767d0e828e299
gregk27 <git@gregk.ca>
2020-07-29 20:43:03 -0400

    Updated webview.ts to support scripts

--- src/creators/webView.ts
+++ src/creators/webView.ts
@@ -3,10 +3,13 @@ import * as fs from 'fs';
 import { getServers } from 'dns';
 
 const cssPattern = /<link\s*rel=['"]stylesheet['"]\s*type="text\/css"\s*href=['"](\w*\.css)['"]\s*>/g;
+const scriptPattern = /<script\s*src=['"](\w*\.js)['"]\s*>/g;
 
 export abstract class WebviewBase {
 
-    options: vscode.WebviewPanelOptions & vscode.WebviewOptions = {};
+    options: vscode.WebviewPanelOptions & vscode.WebviewOptions = {
+        enableScripts:true
+    };
     private html:string;
 
     constructor(
@@ -37,6 +40,13 @@ export abstract class WebviewBase {
             if(filename !== undefined){
                 return substring.replace(filename, this.getResource(filename, panel.webview));
                 // return "<style>\n"+fs.readFileSync(this.filePath.substring(0, this.filePath.lastIndexOf('/')+1)+filename)+"\n</style>";
+            }
+            return substring;
+        });
+        this.html = this.html.replace(scriptPattern, (substring, filename)=>{
+            console.log(substring, filename);
+            if(filename !== undefined){
+                return substring.replace(filename, this.getResource(filename, panel.webview));
             }
             return substring;
         });
