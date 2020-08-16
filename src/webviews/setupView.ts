ab5b585de9738d25af7b298d978a4c3065ad3680
gregk27 <git@gregk.ca>
2020-08-16 16:44:19 -0400

    Config file now created with options from wizard

--- src/webviews/setupView.ts
+++ src/webviews/setupView.ts
@@ -1,6 +1,7 @@
 import { WebviewBase } from "./webView";
 import * as vscode from "vscode";
 import * as fs from "fs";
+import { webview } from "resources/html/scripts/common";
 
 export class SetupView extends WebviewBase {
 
@@ -8,9 +9,24 @@ export class SetupView extends WebviewBase {
         super(context, "setupBotbuilder", "Initialize Botbuilder", "setup.html");
     }
 
+    onMessage(message:webview.Message, panel:vscode.WebviewPanel){
+        if(message.id === "submit"){
+            this.createConfig(message.payload);
+        }
+    }
+
     public getHTML():string{
         let configSchema = fs.readFileSync(__dirname+"/../../../resources/config.schema.json").toString().replace(/\r?\n|\r/g, "").replace(/\\/g, "\\\\").replace(/'/g, "\\'");
         return this.html.replace("${CONFIG_SCHEMA}", configSchema);
     }
 
+    createConfig(payload: {[key:string]: webview.InputState}){
+        let out:{[key:string]: any} = {};
+        for(let [key, input] of Object.entries(payload)){
+            out[key] = input.data;
+        }
+        console.log(JSON.stringify(out, null, 4));
+        fs.writeFileSync(vscode.workspace.rootPath + "/" + vscode.workspace.getConfiguration("ler-botbuilder").get("configPath"), 
+            JSON.stringify(out, null, 4));
+    }
 }
