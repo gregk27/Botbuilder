0b0b920f9d66dd30a4bb466fef325394e4e3a3ff
gregk27 <git@gregk.ca>
2020-08-17 12:33:31 -0400

    Added option to install FRCMocks during initialization

--- src/webviews/setupView.ts
+++ src/webviews/setupView.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import { webview } from "resources/html/scripts/common";
 import { Linkable } from "../treeView/codeElements";
-import { activate } from "../extension";
+import * as cp from 'child_process';
 
 export class SetupView extends WebviewBase {
 
@@ -35,13 +35,34 @@ export class SetupView extends WebviewBase {
     createConfig(payload: {[key:string]: webview.InputState}):string{
         let out:{[key:string]: any} = {};
         for(let [key, input] of Object.entries(payload)){
+            if(key === "addMocks"){
+                continue;
+            }
             out[key] = input.data;
         }
 
         out.hardware = JSON.parse(fs.readFileSync(__dirname+"/../../../resources/defaultHardware.json").toString().replace(/\r?\n|\r/g, "").replace(/\\/g, "\\\\").replace(/'/g, "\\'")).hardware;
        
+        if(payload["addMocks"].data === true){
+            this.addMocks();
+        }
         let file = vscode.workspace.rootPath + "/" + vscode.workspace.getConfiguration("ler-botbuilder").get("configPath");
         fs.writeFileSync(file, JSON.stringify(out, null, 4));
         return file;
+    }
+
+    addMocks(){
+        console.log("Adding mocks");
+        let file = fs.readFileSync(vscode.workspace.rootPath+"/build.gradle").toString();
+        if(!file.includes("ca.gregk:frcmocks")){
+            file = file.replace(/dependencies\s*{/g, "dependencies {\n    implementation 'ca.gregk:frcmocks:+'\n");
+        }
+        if(file.includes("repositories")){
+            file = file.replace(/repositories\s*{/g, "repositories {\n    maven {\n                    url 'http://gregk.ca/FRCMocks/'\n                }\n");
+        } else {
+            file = file.replace(/(?<=plugins\s{.*?})/s, "\n\nrepositories {\n    maven {\n         url 'http://gregk.ca/FRCMocks/'\n    }\n}");
+        }
+        fs.writeFileSync(vscode.workspace.rootPath+"/build.gradle", file);
+        cp.exec("gradlew install", { cwd:vscode.workspace.rootPath});
     }
 }
